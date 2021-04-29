c55c584caf6c3ce454e69fa5027b060a353ea954
oschku <honkasalo.oskari@gmail.com>
2021-04-29 13:24:05 +0300

    fixed create profile routing issue

--- server/client/src/helpers/CreateProfileRoute.js
+++ server/client/src/helpers/CreateProfileRoute.js
@@ -1,14 +1,13 @@
 import { Route, Redirect } from 'react-router-dom';
 
 export default function CreateProfileRoute ({component: Component, auth, profile, redirect, ...rest}) {
-  var profileExists = false
-  profile === undefined || profile === null || profile === '' ? profileExists = false: profileExists = true
+
     return (
       <Route
         {...rest}
         render={(props) => 
-          auth !== false && !profileExists
-          ? <Component {...props} />
+          profile === false?
+          <Component {...props} />
           : auth === false ? 
           <Redirect to={{pathname: '/signin', state: {from: props.location}}} />
           : <Redirect to={{pathname: '/home', state: {from: props.location}}} />        
--- server/client/src/helpers/PrivateRoute.js
+++ server/client/src/helpers/PrivateRoute.js
@@ -2,18 +2,17 @@ import { Route, Redirect } from 'react-router-dom';
 
 export default function PrivateRoute ({component: Component, auth, profile, ...rest}) {
     
-  var profileExists = false
-  profile === undefined || profile === null || profile === '' ? profileExists = false: profileExists = true
+  var profileExists = profile
   console.log('profileExists', profileExists)
   return (
       <Route
         {...rest}
         render={(props) => 
-          auth !== false && profileExists? 
+          auth !== false && profileExists !== false? 
           <Component {...props} />
           : auth === false ?
           <Redirect to={{pathname: '/signin', state: {from: props.location}}} />
-          : !profileExists ? 
+          : profileExists === false ? 
           <Redirect to={{pathname: '/profile/create', state: {from: props.location}}} />
           : <Redirect to={{pathname: '/', state: {from: props.location}}} />
         
