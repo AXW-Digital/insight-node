43a7f2daee1ec4b3d10dd79f2ac9904ce79216af
oschku <honkasalo.oskari@gmail.com>
2021-09-16 08:58:58 +0300

    updated adminpanel ip address

--- server/client/src/config/keys.js
+++ server/client/src/config/keys.js
@@ -1,7 +1,7 @@
 const keys = {
     googleTrackingID: 'UA-199658443-1',
-    adminUrl: 'http://13.48.147.133:3030',
-    localUrl: 'http://13.53.74.148:3030'
+    adminUrl: 'http://ec2co-ecsel-1n5taiqogsorw-561415610.eu-north-1.elb.amazonaws.com:3030',
+    localUrl: 'http://ec2co-ecsel-1n5taiqogsorw-561415610.eu-north-1.elb.amazonaws.com:3030'
 }
 
 export default keys
