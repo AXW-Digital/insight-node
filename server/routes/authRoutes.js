41e9254d98c1fa95a602159ef8ed92c78a1a8716
oschku <oskari.honkasalo@growflow.fi>
2022-02-21 12:32:59 +0200

    added external google auth for webview

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,46 +1,79 @@
-const passport = require('passport');
+const passport = require("passport");
+var request = require("request");
+const axios = require("axios");
 
-module.exports = app => {
+module.exports = (app) => {
   app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
-    }),
-  );  
-
-  app.get(
-    '/auth/google/callback', 
-    passport.authenticate('google'),
-    (req, res) => {     
-      console.log(req.user._id, 'logged in at ', Date(Date.now())) 
-      res.redirect('/home');
-    }
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
   );
 
   app.get(
-    '/auth/facebook',
-    passport.authenticate('facebook',
-    { scope: ['email'] })
-  );
-
-  app.get(
-    '/auth/facebook/callback', 
-    passport.authenticate('facebook'),
+    "/auth/google/callback",
+    passport.authenticate("google"),
     (req, res) => {
-      console.log(req.user._id, 'logged in at ', Date(Date.now()))       
-      res.redirect('/home');
+      console.log(req.user._id, "logged in at ", Date(Date.now()));
+      res.redirect("/home");
     }
   );
-    
 
+  app.get(
+    "/auth/facebook",
+    passport.authenticate("facebook", { scope: ["email"] })
+  );
 
-  app.get('/api/logout', (req, res) => {
+  app.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook"),
+    (req, res) => {
+      console.log(req.user._id, "logged in at ", Date(Date.now()));
+      res.redirect("/home");
+    }
+  );
+
+  app.get("/api/logout", (req, res) => {
     req.logout();
-    res.redirect('/');
+    res.redirect("/");
   });
 
   // test to see if the oauth works with the current user
-  app.get('/api/current_user', (req, res) => {
+  app.get("/api/current_user", (req, res) => {
     res.status(200).send(req.user);
   });
+
+
+
+  app.post(
+    "/auth/google/token",
+    passport.authenticate("google-oauth-token"),
+    (req, res) => {
+      console.log(req.user)
+      // do something with req.user
+      res.send(req.user ? 200 : 401);
+    }
+  );
+
+  app.post(
+    "/auth/facebook/token",
+    passport.authenticate("passport-facebook-token"),
+    (req, res) => {
+      console.log(req.body)
+      // do something with req.user
+      res.send(req.user ? 200 : 401);
+    }
+  );
+
+
+//   app.get("/auth/google/token", async (req, res) => {
+//     const uri = `/auth/google/token?${req.query.access_token}`    
+//   }).then((response) => {
+//     await axios.post(uri).then(
+//       res.status(200)
+//     )
+// });;
+
+
+
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,30 +1,30 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const BearerStrategy = require("passport-http-bearer").Strategy;
+const GoogleOauthTokenStrategy = require("passport-google-oauth-token");
+const FacebookTokenStrategy = require("passport-facebook-token");
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
+  User.findById(id).then((user) => {
     done(null, user);
-  })
+  });
 });
-
-
-
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
+      callbackURL: "/auth/google/callback",
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
@@ -37,29 +37,73 @@ passport.use(
       const user = await new User({ googleId: profile.id }).save();
       done(null, user);
     }
-  ));
+  )
+);
 
 passport.use(
   new FacebookStrategy(
     {
       clientID: keys.facebookClientID,
       clientSecret: keys.facebookClientSecret,
-      callbackURL: '/auth/facebook/callback',
+      callbackURL: "/auth/facebook/callback",
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ facebookId: profile.id });
 
       if (existingUser) {
-        console.log("found fb user: ", profile.id)
+        console.log("found fb user: ", profile.id);
         return done(null, existingUser);
       } else {
-        console.log("found no fb user, creating new one: ", profile.id)
-        const user = await new User({ facebookId: profile.id.toString() }).save();
+        console.log("found no fb user, creating new one: ", profile.id);
+        const user = await new User({
+          facebookId: profile.id.toString(),
+        }).save();
         done(null, user);
       }
-
-      
     }
-  ));
+  )
+);
 
+passport.use(
+  new GoogleOauthTokenStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
+
+passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      fbGraphVersion: "v3.0",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ facebookId: profile.id });
+
+      if (existingUser) {
+        console.log("found fb user: ", profile.id);
+        return done(null, existingUser);
+      } else {
+        console.log("found no fb user, creating new one: ", profile.id);
+        const user = await new User({
+          facebookId: profile.id.toString(),
+        }).save();
+        done(null, user);
+      }
+    }
+  )
+);
